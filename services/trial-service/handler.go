3c455158b9a8a53c4ae1487ced9f868aa6818dea
Vinícius Lavrador <vinicius.lavrador@icloud.com>
2022-06-10 12:31:57 -0300

    Added base batch download to trial service

--- services/trial-service/handler.go
+++ services/trial-service/handler.go
@@ -6,23 +6,36 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/obrafy/planning/infrastructure/sqsbase"
-	"github.com/sirupsen/logrus"
 )
 
 func (trialService *TrialService) Handler(msg *sqsbase.SQSMessage) error {
-	log := logrus.WithFields(logrus.Fields{"incoming-message": msg})
+	// log := logrus.WithFields(logrus.Fields{"incoming-message": msg})
 
-	fileBuffer := new(aws.WriteAtBuffer)
-
-	trialService.S3ManagerClient.Downloader.Download(fileBuffer, &s3.GetObjectInput{
+	// Batch Download
+	files, err := trialService.S3ManagerClient.Downloader.S3.ListObjects(&s3.ListObjectsInput{
 		Bucket: trialService.S3ManagerClient.Bucket,
-		Key:    aws.String("planning-files/new_user_credentials-3.csv"),
+		Prefix: aws.String("planning-files"),
 	})
 
-	fmt.Println(string(fileBuffer.Bytes()))
+	if err != nil {
+		return fmt.Errorf("error listing files from s3 folder: %w", err)
+	}
 
-	log.Info("New Trial Message")
-	fmt.Println("New Trial Message In Service Handler")
+	for _, file := range files.Contents {
+		// Single Object Download
+
+		fileBuffer := new(aws.WriteAtBuffer)
+
+		trialService.S3ManagerClient.Downloader.Download(fileBuffer, &s3.GetObjectInput{
+			Bucket: trialService.S3ManagerClient.Bucket,
+			Key:    aws.String(*file.Key),
+		})
+
+		fmt.Println(string(fileBuffer.Bytes()))
+
+	}
+
+	// fmt.Println(files)
 
 	return nil
 }
